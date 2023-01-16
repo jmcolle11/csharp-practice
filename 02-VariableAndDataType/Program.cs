2936148fd05b17398fd0242519a3e0a09156ad28
John Michael Colle <jmqc1211@gmail.com>
2023-01-16 16:21:56 +0800

    Modify the syntax

--- 02-VariableAndDataType/Program.cs
+++ 02-VariableAndDataType/Program.cs
@@ -5,8 +5,6 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, JM");
-            Console.WriteLine("Test Commit!");
         }
     }
 }
